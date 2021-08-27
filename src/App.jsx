13f832807d7e82d5a97da3e9df4417a3a7376aaa
ddominan <ddominan12@gmail.com>
2021-08-27 13:40:09 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+        <h1>Online CV</h1>
+        <h2>Dan Dominan</h2>
+    </div>
+  )
+}
+
+export default App
